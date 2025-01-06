87a226be9b1f6e7f479063e3978c1f6cafac3e9e
vikranko <vikrant.kotkar@amdocs.com>
2025-01-06 08:31:55 +0530

    New code added

---
+++ src/ex_01_Java_Basic/Println_vs_print.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Println_vs_print {
+    public static void main(String[] args) {
+        System.out.println("vikrant");
+        System.out.println("kotkar");
+    }
+}
