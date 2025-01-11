ebd96962531fa783ae14b2a3e8493de4bb4b4e3b
vikranko <vikrant.kotkar@amdocs.com>
2025-01-11 12:19:54 +0530

    11/01/2025

---
+++ src/ex_01_Java_Basic/lab008_Comments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+public class lab008_Comments {
+    public static void main(String[] args) {
+        System.out.println("Comments:");
+        // this is single line comment
+        System.out.println("Single line comment");
+        /* this is multi-line comments.
+        it will be ignored by JVM/compiler */
+        System.out.println("multi line comment");
+    }
+}
